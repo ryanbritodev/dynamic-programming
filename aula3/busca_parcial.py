eb81ae94c78787bca1121114e05fe2391891e6fc
Ryan Brito <ryanbritoramos@gmail.com>
2025-04-28 21:55:35 -0300

    add: Uso de SET

--- aula3/busca_parcial.py
+++ aula3/busca_parcial.py
@@ -9,3 +9,24 @@ maiores = heapq.nlargest(n, lista)
 
 print(menores)
 print(maiores)
+
+
+dicionario = {
+    "A": ["B", "C"],
+    "B": ["A", "C", "D", "E"],
+    "C": ["A", "B", "D"],
+    "D": ["D", "C", "E"],
+    "E": ["B", "D"]
+}
+
+
+caminhos_dict = set(dicionario["C"])
+caminhos_dict2 = set(dicionario["E"])
+
+caminhos_em_comum = caminhos_dict & caminhos_dict2
+
+print(caminhos_em_comum)
+
+caminhos_diferentes = caminhos_dict - caminhos_dict2
+
+print(caminhos_diferentes)
